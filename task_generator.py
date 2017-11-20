77ac4f11639fa53ac8d96ea880e5b4eb0bbef682
mmalik <mmalik@localhost.com>
2017-11-20 14:48:53 -0800

    accept command line parameters; random task names must be unique

--- task_generator.py
+++ task_generator.py
@@ -4,6 +4,7 @@ The generator script inserts 100 random tasks into mongodb.
 import string
 import random
 import pymongo
+import sys
 from pymongo import MongoClient
 from random import randint
 
@@ -11,16 +12,32 @@ NUM_TASKS = 100
 TASK_NAME_LENGTH = 32
 MAX_TASK_DURATION_IN_SECS = 60
 
-client = MongoClient('172.17.0.2', 27017)
+if (len(sys.argv) != 3):
+    print("usage: python task_generator.py hostname port")
+
+hostname = sys.argv[1]
+port = int(sys.argv[2])
+
+client = MongoClient(hostname, port)
 db = client['scheduler']
 tasks = db.tasks
+tasknames = []
 
-for i in range(0,NUM_TASKS):
-    taskname = ''.join(random.choice(string.ascii_lowercase) for _ in range(TASK_NAME_LENGTH))
-    sleeptime = randint(0, MAX_TASK_DURATION_IN_SECS)
+def random_unique_task_name():
+    while True:
+        taskname = ''.join(random.choice(string.ascii_lowercase) for _ in range(TASK_NAME_LENGTH))
+        if (taskname not in tasknames):
+            tasknames.append(taskname)
+            return taskname
+
+def random_sleep_time():
+    return randint(0, MAX_TASK_DURATION_IN_SECS)
+
+for i in range(0, NUM_TASKS):
+    taskname = random_unique_task_name()
+    sleeptime = random_sleep_time()
     task = {"taskname": taskname, "sleeptime": sleeptime, "state": "created", "host": ""}
     tasks.insert_one(task)
-
 
 
 
