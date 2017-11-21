545d420a8c2ad3a41c30f78ce50cd79a4e3055f4
mmalik <mmalik@localhost.com>
2017-11-21 11:25:16 -0800

    docker run commands for master and slave

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ mongo_port=27017
 # run task generator script
 $(python task_generator.py $mongo_host $mongo_port)
 
+# run master
+#sudo docker run -d master python /opt/scheduler/master.py $mongo_host $mongo_port
 
+# run slave
+#sudo docker run -d slave python /opt/scheduler/slave.py
