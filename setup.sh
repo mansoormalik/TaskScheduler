a410d934501a6f9622096acf805db42d9e9279c9
mmalik <mmalik@localhost.com>
2017-11-21 18:24:35 -0800

    add functionality to kill slave nodes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+docker_run="sudo docker run --log-driver=fluentd -d"
+
+# run fluentd container
+#sudo docker run -it -p 24224:24224 -v $PWD/fluentd.conf:/fluentd/etc/fluentd.conf -e FLUENTD_CONF=fluentd.conf fluent/fluentd:latest
+
 # run mongodb docker container
 sudo docker run -d mongo
 mongo_container=$(sudo docker ps -l -q)
@@ -8,7 +15,36 @@ mongo_port=27017
 $(python task_generator.py $mongo_host $mongo_port)
 
 # run master
-#sudo docker run -d master python /opt/scheduler/master.py $mongo_host $mongo_port
+master_run="python /opt/scheduler/master.py $mongo_host $mongo_port"
+$docker_run master $master_run
+master_container=$(sudo docker ps -l -q)
+master_host=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $master_container)
 
 # run slave
-#sudo docker run -d slave python /opt/scheduler/slave.py
+max_slaves=4
+slave_nr=0
+slave_containers=()
+for i in `seq  $slave_nr $(($max_slaves-1))`; do
+    $docker_run slave python /opt/scheduler/slave.py $master_host slave-$slave_nr
+    slave_container=$(sudo docker ps -l -q)
+    slave_containers+=($slave_container)
+    slave_nr=$(($slave_nr+1))
+done
+
+for slave_container in "${slave_containers[@]}"; do
+    echo $slave_container
+done
+
+# kill worker
+echo '{"source":"driver","message":"killing worker"}' | fluent-cat stdout
+
+max_sleep_in_secs=5
+for index in `seq 0 1`; do
+    sleep $(($RANDOM % max_sleep_in_secs))
+    idx=$(($RANDOM % max_slaves))
+    slave_container=${slave_containers[$idx]}
+    #echo "{"message":"stopping container $slave_container"}" | fluent-cat stdout
+    echo "stopping container $slave_container associated with slave-$idx"
+    sudo docker stop $slave_container
+    sleep 5
+done
