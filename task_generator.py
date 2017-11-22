51ffe751720fa2f37d052a49d8656ee3788e5528
mmalik <mmalik@localhost.com>
2017-11-21 18:24:03 -0800

    refactoring

--- task_generator.py
+++ task_generator.py
@@ -1,6 +1,5 @@
-"""
-The generator script inserts 100 random tasks into mongodb.
-"""
+""" The generator script inserts 100 random tasks into mongodb."""
+
 import string
 import random
 import pymongo
@@ -8,7 +7,8 @@ import sys
 from pymongo import MongoClient
 from random import randint
 
-NUM_TASKS = 5
+# modify parameters as required
+NUM_TASKS = 20
 TASK_NAME_LENGTH = 32
 MAX_TASK_DURATION_IN_SECS = 10
 
@@ -31,7 +31,7 @@ def random_unique_task_name():
             return taskname
 
 def random_sleep_time():
-    return randint(0, MAX_TASK_DURATION_IN_SECS)
+    return randint(1, MAX_TASK_DURATION_IN_SECS)
 
 for i in range(0, NUM_TASKS):
     taskname = random_unique_task_name()
