5250dd2d1fba65c98ec8b45ef673945ac32be9eb
mmalik <mmalik@localhost.com>
2017-11-22 07:31:54 -0800

    cleanup master and slave

--- master.py
+++ master.py
@@ -5,6 +5,7 @@ import masterslave_pb2_grpc
 import sys
 import pymongo
 import logging
+import threading
 from pymongo import MongoClient
 from concurrent import futures
 from collections import deque
@@ -22,36 +23,62 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.info("Master is launching")
 
-_ONE_DAY_IN_SECONDS = 60 * 60 * 24
-
+CHECK_TASKS_INTERVAL_IN_SECS = 10
 client = MongoClient(host, port)
 db = client['scheduler']
-unassigned_taskname_to_task = {}
+taskname_to_task = {}
 unassigned_tasks = deque()
-
-for task in db.tasks.find({}):
-    unassigned_taskname_to_task[task['taskname']] = task
-    unassigned_tasks.append(task)
+tasklock = threading.Lock()
 
 class Master(masterslave_pb2_grpc.TaskSchedulerServicer):
-    def SendTask(self, request, context):
+    def Task(self, request, context):
         if (len(unassigned_tasks) > 0):
+            tasklock.acquire()
             task = unassigned_tasks.pop()
             id = task['_id']
+            taskname = task['taskname']
+            sleeptime = task['sleeptime']
             task['host'] = request.slaveid
+            task['state'] = "running"
             db.tasks.update_one({'_id':id}, {"$set":task}, upsert=False)
-            logger.info("Master is placing task %s on slave", task['taskname'])
-            return masterslave_pb2.TaskResponse(taskname=task['taskname'], sleeptime=task['sleeptime'])
+            tasklock.release()
+            logger.info(f"Master is assigning task {taskname} to {request.slaveid}")
+            return masterslave_pb2.TaskResponse(taskname=taskname, sleeptime=sleeptime)
         else:
             return masterslave_pb2.TaskResponse(taskname="", sleeptime=0)
     
-    def SendStatus(self, request, context):
-        task = unassigned_taskname_to_task[request.taskname]
+    def Status(self, request, context):
+        tasklock.acquire()
+        task = taskname_to_task[request.taskname]
         id = task['_id']
         task['state'] = 'success'
         db.tasks.update_one({'_id':id}, {"$set":task}, upsert=False)
-        return masterslave_pb2.StatusResponse(taskname="ok")
+        tasklock.release()
+        return masterslave_pb2.StatusResponse()
 
+def obtain_unassigned_tasks():
+    tasklock.acquire()
+    for task in db.tasks.find({"state":"created"}):
+        taskname = task['taskname']
+        if (taskname not in taskname_to_task):
+            unassigned_tasks.append(task)
+            taskname_to_task[task['taskname']] = task
+    tasklock.release()
+
+def obtain_killed_tasks():
+    tasklock.acquire()
+    for task in db.tasks.find({"state":"killed"}):
+        taskname = task['taskname']
+        if (taskname in taskname_to_task):
+            existing = taskname_to_task[taskname]
+            if (existing['state'] != "killed"):
+                unassigned_tasks.append(task)
+                taskname_to_task[taskname] = task
+        else:
+            unassigned_tasks.append(task)
+            taskname_to_task[taskname] = task
+    tasklock.release()
+        
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     masterslave_pb2_grpc.add_TaskSchedulerServicer_to_server(Master(), server)
@@ -59,10 +86,14 @@ def serve():
     server.start()
     try:
         while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
+            time.sleep(CHECK_TASKS_INTERVAL_IN_SECS)
+            obtain_unassigned_tasks()
+            obtain_killed_tasks();
     except KeyboardInterrupt:
         server.stop(0)
     
 if __name__ == '__main__':
+    obtain_unassigned_tasks()
+    obtain_killed_tasks()
     serve()
             
--- slave.py
+++ slave.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-
 import grpc
 import time
 import logging
@@ -13,29 +12,34 @@ if (len(sys.argv) != 3):
 host = sys.argv[1]
 id = sys.argv[2]
 
+POLLING_INTERVAL_NO_TASKS_IN_SECS = 3
 FORMAT = '%(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.info(id + " is launching")
 
-def execute_task(taskname, sleeptime, stub):
-  if (sleeptime == 0):
-    time.sleep(1)
-  else:
-    logger.info(id + " started task %s", taskname)
-    time.sleep(sleeptime)
-    logger.info(id + " completed task %s", taskname)
-    stub.SendStatus(masterslave_pb2.StatusRequest(slaveid=id, taskname=taskname))
 
+# if task queue was empty master will send empty taskname
+# slave should wait for configurable interval and poll again
+def execute_task(taskname, sleeptime, stub):
+    if (len(taskname) == 0):
+        time.sleep(POLLING_INTERVAL_NO_TASKS_IN_SECS)
+    else:
+        logger.info(f"{id} started task {taskname}")
+        time.sleep(sleeptime)
+        logger.info(f"{id} completed task {taskname}")
+        stub.Status(masterslave_pb2.StatusRequest(slaveid=id, taskname=taskname))
+        
 def run():
-  channel = grpc.insecure_channel(host + ':50051')
-  stub = masterslave_pb2_grpc.TaskSchedulerStub(channel)
-  while True:
-    response = stub.SendTask(masterslave_pb2.TaskRequest(slaveid=id))
-    taskname = response.taskname
-    sleeptime = response.sleeptime
-    execute_task(taskname, sleeptime, stub)
+    channel = grpc.insecure_channel(host + ':50051')
+    grpc.channel_ready_future(channel).result()
+    stub = masterslave_pb2_grpc.TaskSchedulerStub(channel)
+    while True:
+        response = stub.Task(masterslave_pb2.TaskRequest(slaveid=id))
+        taskname = response.taskname
+        sleeptime = response.sleeptime
+        execute_task(taskname, sleeptime, stub)
 
 if __name__ == '__main__':
-  run()
+    run()
