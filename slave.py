9df09d67a3a82ebc170079be0fce6ce8befbc299
mmalik <mmalik@localhost.com>
2017-11-21 18:23:31 -0800

    assign id to slave node; refactor exec task function

--- slave.py
+++ slave.py
@@ -7,31 +7,35 @@ import sys
 import masterslave_pb2
 import masterslave_pb2_grpc
 
-if (len(sys.argv) != 2):
-    print("usage: python slave.py host")
+if (len(sys.argv) != 3):
+    print("usage: python slave.py host slaveid")
     sys.exit(1)
 host = sys.argv[1]
+id = sys.argv[2]
 
 FORMAT = '%(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logger.info("Slave is launching")
+logger.info(id + " is launching")
 
-def execute_task(taskname, sleeptime):
-  logger.info("Slave started task %s", taskname)
-  time.sleep(sleeptime)
-  logger.info("Slave completed task %s", taskname)
+def execute_task(taskname, sleeptime, stub):
+  if (sleeptime == 0):
+    time.sleep(1)
+  else:
+    logger.info(id + " started task %s", taskname)
+    time.sleep(sleeptime)
+    logger.info(id + " completed task %s", taskname)
+    stub.SendStatus(masterslave_pb2.StatusRequest(slaveid=id, taskname=taskname))
 
 def run():
   channel = grpc.insecure_channel(host + ':50051')
   stub = masterslave_pb2_grpc.TaskSchedulerStub(channel)
-  for i in range(5):
-    response = stub.SendTask(masterslave_pb2.TaskRequest(slaveid='slave1'))
+  while True:
+    response = stub.SendTask(masterslave_pb2.TaskRequest(slaveid=id))
     taskname = response.taskname
     sleeptime = response.sleeptime
-    execute_task(taskname, sleeptime)
-    response = stub.SendStatus(masterslave_pb2.StatusRequest(slaveid='slave1', taskname=taskname))
+    execute_task(taskname, sleeptime, stub)
 
 if __name__ == '__main__':
   run()
