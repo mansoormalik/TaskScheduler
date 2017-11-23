a60bd8b9f809b6b48076ba72780aac01115ef488
mmalik <mmalik@localhost.com>
2017-11-23 13:02:47 -0800

    cleanup docker container and images when doing builds

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,8 @@
-# Create docker images for master and slave
-
+# Clean up and create docker images for master and slave
+sudo docker kill $(sudo docker ps -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker rmi $(sudo docker images -f "dangling=true" -q)
+sudo docker rmi master
 sudo docker build -f Dockerfile.master -t master .
+sudo docker rmi slave
 sudo docker build -f Dockerfile.slave -t slave .
